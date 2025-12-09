e36f3c88e4b4e5faebd0c0ed5c0a454d67b2e5f2
Uwase Djaria <uwasedjaria41@gmail.com>
2025-12-09 14:15:50 +0200

    update styles by adding react icons

--- components/navigation/ParentNavbar.tsx
+++ components/navigation/ParentNavbar.tsx
@@ -1,12 +1,16 @@
 import Link from 'next/link'
 import React from 'react'
 import { FaSignOutAlt } from 'react-icons/fa'
+import { LuBus } from 'react-icons/lu'
 
 function ParentNavbar() {
   return (
     <div>
         <nav className='font-sans bg-white flex sm:flex-row flex-col items-center justify-between gap-4 sm:gap-6 py-4 sm:py-6 px-4 sm:px-8 shadow-lg'>
+        <div className='flex items-center gap-3'>
+        <LuBus className='bg-blue-500 rounded-lg p-2 h-10 w-10 text-white'/>
         <h1 className='text-2xl sm:text-3xl md:text-4xl font-bold text-gray-800'>My Dashboard</h1>
+          </div>
         <div className='flex flex-wrap items-center justify-center font-medium text-xs sm:text-sm gap-3 sm:gap-6'>
         <Link href='/dashboard' className='text-gray-600 hover:text-black'>Dashboard</Link>
         
--- components/navigation/maps/parentDashboardMap.tsx
+++ components/navigation/maps/parentDashboardMap.tsx
@@ -10,9 +10,13 @@ function ParentDashboardMap() {
   const position: LatLngExpression = [-1.9441, 30.0619]
   
   return (
-    <div>
+
     <div className="w-10/12 mx-auto mt-5 h-[400px] flex justify-end">
-      <div className="w-1/2 h-full bg-blue-200 rounded-lg shadow-lg overflow-hidden">
+        <div className='border w-200 p-4 rounded-lg shadow-lg bg-blue-100 overflow-hidden'>
+        <div className="flex items-center gap-2">
+        <div className="h-4  w-1 mb-4 bg-blue-700 rounded-lg"></div>
+         <h2 className='text-xl font-semibold mb-4 text-black  text-left p-2'>Live Bus Location</h2>
+         </div>
         <MapContainer 
             center={position}
             zoom={14}
@@ -25,7 +29,9 @@ function ParentDashboardMap() {
         </MapContainer>
       </div>
     </div>
-    </div>
+    
+    
+   
   )
 }
 
