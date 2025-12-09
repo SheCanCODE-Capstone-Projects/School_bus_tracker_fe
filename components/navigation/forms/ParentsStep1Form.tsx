0c966890cab366a3d251bd1951c95ccf3599874b
Mukdaniella <mugabodaniella22@gmail.com>
2025-12-09 15:18:21 +0200

    Add parent registration form step 1

--- components/navigation/forms/ParentsStep1Form.tsx
+++ components/navigation/forms/ParentsStep1Form.tsx
@@ -10,7 +10,7 @@ export default function ParentRegistration() {
           </div>
         </div>
 
-        <h1 className="mt-3 text-[22px] text-black font-semibold">
+        <h1 className="mt-3 text-22px text-black font-semibold">
           Parent Registration
         </h1>
         <p className="text-gray-500 text-sm">
@@ -18,7 +18,25 @@ export default function ParentRegistration() {
         </p>
       </div>
 
-     
+    <div className="flex justify-center gap-8 mt-5">
+  <div className="text-center">
+  <div className="w-10 h-10 bg-blue-600 rounded-full flex items-center justify-center">
+    <span className="text-white text-lg font-bold">1</span>
+  </div>
+  <p className="text-xs text-blue-600">Parent Info</p>
+</div>
+
+
+  <div className="w-10 h-1 mt-5 bg-gray-400"></div>
+
+  <div className="text-center opacity-50">
+    <div className="w-10 h-10 bg-gray-200 rounded-full flex items-center justify-center text-gray-900">
+      2
+    </div>
+    <p className="text-xs text-gray-900">Children Info</p>
+  </div>
+</div>
+
     </div>
   );
 }
