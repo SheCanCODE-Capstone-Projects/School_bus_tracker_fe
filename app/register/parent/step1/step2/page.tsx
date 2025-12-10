09917113ac9f7624518171f2ba3391d493bd935b
Mukdaniella <mugabodaniella22@gmail.com>
2025-12-10 15:53:45 +0200

    i added the validation and responsiveness

--- app/register/parent/step1/step2/page.tsx
+++ app/register/parent/step1/step2/page.tsx
@@ -16,12 +16,15 @@ export default function ChildrenRegistration() {
 
   return (
     <div className="p-5 bg-gray-100 min-h-screen">
-      {/* Header */}
       <div className="text-center mt-5">
         <div className="inline-flex items-center justify-center w-20 h-20 bg-blue-500 rounded-2xl shadow-lg mb-4">
           <Bus className="w-12 h-12 text-white" strokeWidth={2.5} />
         </div>
-
+        <div>
+          <h1 className="text-gray-900 text-3xl font-bold">
+            SCHOOL BUS TRACKER
+          </h1>
+        </div>
         <h1 className="mt-3 text-2xl text-black font-semibold">
           Parent Registration
         </h1>
@@ -57,7 +60,7 @@ export default function ChildrenRegistration() {
         </div>
       </div>
 
-<div className="bg-white mt-8 mx-auto p-5 rounded-lg max-w-xl shadow-md">
+      <div className="bg-white mt-8 mx-auto p-5 rounded-lg max-w-xl shadow-md">
         <div className="flex justify-between items-center mb-4">
           <h2 className="text-lg font-semibold text-black">
             Children Information
@@ -149,11 +152,6 @@ export default function ChildrenRegistration() {
           </div>
         </form>
       </div>
-
-
-      
     </div>
   );
 }
-
-
